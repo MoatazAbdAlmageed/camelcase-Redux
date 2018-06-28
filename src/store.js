7fb76e8dc9eca002d91225becdca0823c05e5874
moataz <moataz.mohammady@gmail.com>
2018-06-28 18:32:05 +0200

    Remove unused lines

--- src/store.js
+++ src/store.js
@@ -1,20 +1,9 @@
 import { createStore } from 'redux'
-// import applyMiddleware from "redux/src/applyMiddleware";
-
 import reducer from './reducers'
-//
-// const logger = (store)=>(next)=>(action)=>{
-//     console.log(action);
-//     next(action)
-// }
-// // const middleware = applyMiddleware(logger);
-
- // const store =  createStore(reducer,1,middleware)
 const store =  createStore(reducer)
 
 
 store.subscribe(()=>{
-
     console.log("Store changed", store.getState());
 })
 
