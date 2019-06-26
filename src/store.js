f69246dda6268c602ca39280760999d123edf237
Moataz <moataz.mohammady@gmail.com>
2019-06-26 16:19:32 +0200

    - applyMiddleware(`logger`) - Update `.gitignore`

--- src/store.js
+++ src/store.js
@@ -1,12 +1,21 @@
-import {createStore} from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers'
+const logger = store => next => action => {
+    console.log('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    return result
+}
+const store = createStore(
+    reducers /* preloadedState */,
+    applyMiddleware( logger /* ...middleware */) /* any number of middlewares as arguments */
+);
 
-const store = createStore(reducers /*Original state*/, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+// const store = createStore(reducers /*Original state*/, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 /*subscribe*/
 store.subscribe(() => {
-    console.log("Store changed", store.getState());
+    // console.log("Store changed", store.getState());
 });
 
 export default store
