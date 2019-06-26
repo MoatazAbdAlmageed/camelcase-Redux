4dc0edc7c2c53fc2f8434a1261c05e5b275a2d4b
Moataz <moataz.mohammady@gmail.com>
2019-06-26 16:43:15 +0200

    Log only in development

--- src/store.js
+++ src/store.js
@@ -1,17 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers'
-const logger = store => next => action => {
-    console.log('dispatching', action)
-    let result = next(action)
-    console.log('next state', store.getState())
-    return result
-}
-const store = createStore(
-    reducers /* preloadedState */,
-    applyMiddleware( logger /* ...middleware */) /* any number of middlewares as arguments */
-);
+import { createStore, applyMiddleware ,compose} from 'redux';
+import reducer from './reducers'
 
-// const store = createStore(reducers /*Original state*/, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const middlewares = [];
+
+if (process.env.NODE_ENV === `development`) {
+    const { logger } = require(`redux-logger`);
+
+    middlewares.push(logger);
+}
+
+
+const enhancer = window._s_REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,enhancer(
+    applyMiddleware(...middlewares)
+));
+
 
 /*subscribe*/
 store.subscribe(() => {
