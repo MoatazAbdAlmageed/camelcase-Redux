b0a5d2352dc17c9fa1f541391e11949622795c18
Moataz <moataz.mohammady@gmail.com>
2019-06-26 23:02:24 +0200

    Get books from `api`

--- src/actions/fetchBooksAction.js
+++ src/actions/fetchBooksAction.js
@@ -1,7 +1,33 @@
-import books from '../data/books'
 
 const LIST_BOOKS = "LIST_BOOKS";
 const fetchBooksAction = () => {
+
+     /* TODO CLEAN UP THIS FILE */
+    const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001';
+    var books;
+    console.log(api);
+    let token = localStorage.token
+
+    if (!token)
+        token = localStorage.token = Math.random().toString(36).substr(-8)
+
+    const headers = {
+        'Accept': 'application/json',
+        'Authorization': token
+    }
+
+    fetch(`${api}/contacts`, {
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+    }).then(res => res.json())
+        .then(data => {
+            console.log(data.contacts);
+           var books = data.contacts;
+        })
+
     return {type: LIST_BOOKS, payload: books};
+
 };
 export default fetchBooksAction
--- src/store.js
+++ src/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware ,compose} from 'redux';
 import reducer from './reducers'
+import thunkMiddleware from 'redux-thunk'
 
-
-const middlewares = [];
-
+const middlewares = [thunkMiddleware];
 if (process.env.NODE_ENV === `development`) {
     const { logger } = require(`redux-logger`);
 
@@ -12,10 +11,12 @@ if (process.env.NODE_ENV === `development`) {
 
 
 const enhancer = window._s_REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer,enhancer(
-    applyMiddleware(...middlewares)
+// const store = createStore(reducer,enhancer(
+//     applyMiddleware(...middlewares)
+// ));
+const store = createStore(reducer, /* preloadedState, */ enhancer(
+    applyMiddleware(thunkMiddleware) // lets us dispatch() functions
 ));
-
 
 /*subscribe*/
 store.subscribe(() => {
